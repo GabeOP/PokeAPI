8decb25304f6a2f9ca5b6c7467149760bdf7c4b4
GabeOP <gabrieloliveirap1@hotmail.com>
2022-10-28 01:19:12 -0300

    upgrade js
    adicionado ataques dos pokemons

--- script/script.js
+++ script/script.js
@@ -2,16 +2,22 @@ let txt = document.getElementById('txt');
 let btn = document.getElementById('btn');
 let res = document.getElementById('res');
 let img = document.getElementById('img')
+let atq = document.getElementById('atq')
 
 function pokeAPI(){
     fetch(`https://pokeapi.co/api/v2/pokemon/${txt.value}`)
     .then((response) => response.json())
     .then((corpo) => {
         console.log(corpo)
-        res.innerHTML = corpo.name;
-        img.setAttribute('src', corpo.sprites.front_default)
+        res.innerHTML = `${corpo.name} #${corpo.id}` ;
+        img.setAttribute('src', corpo.sprites.front_default);
+        for(let i= 0; i<corpo.abilities.length; i++){
+            atq.innerHTML += `${corpo.abilities[i].ability.name} <br>`
+        }
+        
     })
 }
+
 
 btn.addEventListener('click',  pokeAPI)
     
