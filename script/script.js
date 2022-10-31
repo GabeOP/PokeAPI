c5509e1d4227e8b64020a800c542c41ec6714573
GabeOP <gabrieloliveirap1@hotmail.com>
2022-10-30 21:36:47 -0300

    mudanças gerais

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
 let txt = document.getElementById('txt');
 let btn = document.getElementById('btn');
 let res = document.getElementById('res');
-let img = document.getElementById('img')
-let atq = document.getElementById('atq')
+let img = document.getElementById('img');
+let atq = document.getElementById('atq');
 
-function pokeAPI(){
-    fetch(`https://pokeapi.co/api/v2/pokemon/${txt.value}`)
+function pokeAPI() {
+    atq.innerHTML = ''
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${txt.value.toLowerCase()}`)
     .then((response) => response.json())
     .then((corpo) => {
         console.log(corpo)
-        res.innerHTML = `${corpo.name} #${corpo.id}` ;
-        img.setAttribute('src', corpo.sprites.front_default);
-        for(let i= 0; i<corpo.abilities.length; i++){
-            atq.innerHTML += `${corpo.abilities[i].ability.name} <br>`
+        res.innerHTML = `${corpo.name} #${corpo.id}`;
+        img.setAttribute('src', corpo['sprites']['versions']['generation-v']['black-white']['animated']['front_default']);
+        for (let i = 0; i < corpo.abilities.length; i++) {
+            atq.innerHTML += `<b>Ataque ${i+1}:</b> ${corpo.abilities[i].ability.name} <br>`
         }
-        
     })
 }
 
+btn.addEventListener('click', pokeAPI);
 
-btn.addEventListener('click',  pokeAPI)
-    
-       
+
+
